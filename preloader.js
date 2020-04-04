3258e96910f6ff1c57fc6f0123b5f8de3d1009cb
lanbazz <62804907+lanbazz@users.noreply.github.com>
2020-04-04 19:53:38 +0100

    send

---
+++ preloader.js
@@ -0,0 +1,6 @@
+var preloader = document.getElementById('preloader-id') ;
+window.addEventListener('load',function() { 
+    setTimeout(function(){preloader.classList.add('demolish');},3500) ;
+     
+
+})
