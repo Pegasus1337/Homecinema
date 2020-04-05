a8d7703e3039c8ae49a5847aae797a52b573aa30
lanbazz <62804907+lanbazz@users.noreply.github.com>
2020-04-05 21:45:47 +0100

    override

--- preloader.js
+++
@@ -1,6 +0,0 @@
-var preloader = document.getElementById('preloader-id') ;
-window.addEventListener('load',function() { 
-    setTimeout(function(){preloader.classList.add('demolish');},2500) ;
-     
-
-})
